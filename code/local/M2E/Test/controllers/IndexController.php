f5f178ac690a37a9eb278c6d159033f2576fa63a
Khanitskiy <khanitskiy@gmail.com>
2017-03-25 10:06:54 +0200

    delete viewAction

--- code/local/M2E/Test/controllers/IndexController.php
+++ code/local/M2E/Test/controllers/IndexController.php
@@ -9,21 +9,4 @@ class M2E_Test_IndexController extends Mage_Core_Controller_Front_Action
         $this->renderLayout();
     }
 
-
-    public function viewAction()
-    {
-        $testId = Mage::app()->getRequest()->getParam('id', 0);
-        $note = Mage::getModel('m2etest/test')->load($testId);
-
-        if ($note->getId() > 0) {
-            $this->loadLayout();
-            $this->getLayout()->getBlock('test.content')->assign(array(
-                "testItem" => $note,
-            ));
-            $this->renderLayout();
-        } else {
-            $this->_forward('noRoute');
-        }
-    }
-
 }
